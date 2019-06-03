b66c3bac547c9c1023b2f93291ff46ed0357993d
Ihaka Pink <ihakapink@gmail.com>
2019-06-04 10:43:34 +1200

    added comments routes

---
+++ server/routes/comments.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const db = require('../db/comments.js')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    db.getComments()
+    .then(comments => {
+        res.send(comments)
+    })
+    .catch(err => {
+        res.status(500).send('DATABASE ERROR: ' + err.message)
+    })
+})
+
+router.post('/', (req, res) => {
+    db.addComment(req.body)
+    .then(comments => {
+        res.send(comments)
+    })
+    .catch(err => {
+        res.send(500).send(err.message)
+    })
+})
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id
+    db.getComment(id)
+    .then(comment => res.send(comment))
+    .catch(err => res.status(500).send(err.message))
+})
+
+router.delete('/', (req, res) => {
+    db.deleteComment(req.body)
+    .then(res.redirect('/comments'))
+})
